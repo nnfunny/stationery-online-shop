41bd677554553cc787f190628fdd7d8588301882
Nam Nguyen <nguyenvietnam2401@gmail.com>
2020-06-16 00:17:27 +1000

    Finished Grid on Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,16 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { css } from "@emotion/core";
+import "./PageStyles/Home.css";
+import {
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image7,
+  Image8,
+} from "./PageStyles/images/Images";
 
 const Home = () => {
   return (
@@ -12,6 +22,46 @@ const Home = () => {
       `}
     >
       <Header />
+      <div className="home-container grid-container">
+        <div
+          className="image-card"
+          style={{
+            backgroundImage: `url(${Image2})`,
+          }}
+        ></div>
+        <div className="image-subcontainer">
+          <div
+            className="image-card"
+            style={{
+              backgroundImage: `url(${Image5})`,
+            }}
+          ></div>
+          <div
+            className="image-card"
+            style={{
+              backgroundImage: `url(${Image7})`,
+            }}
+          ></div>
+        </div>
+        <div
+          className="image-card"
+          style={{
+            backgroundImage: `url(${Image3})`,
+          }}
+        ></div>
+        <div
+          className="image-card"
+          style={{
+            backgroundImage: `url(${Image4})`,
+          }}
+        ></div>
+        <div
+          className="image-card"
+          style={{
+            backgroundImage: `url(${Image8})`,
+          }}
+        ></div>
+      </div>
       <Footer />
     </div>
   );
